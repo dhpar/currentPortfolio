dcbfda7f38aeee24ff8f7f2f4563cad01bc7c8c1
D <davhern@gmail.com>
2026-09-17 15:07:47 -0500

    fix: update basePath in next.config.ts for correct static export

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/public",
+  basePath: "",
   images: {
     unoptimized: true, // next/image needs this for static export
   },
